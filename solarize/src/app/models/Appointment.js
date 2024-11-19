4d8a332798be17695065acd5dda0d24fb27c6639
Gary Smith <garysmith193358@gmail.com>
2024-11-19 17:08:31 -0500

    Created Appointment schema

---
+++ solarize/src/app/models/Appointment.js
@@ -0,0 +1,28 @@
+import { mongoose, Schema }  from 'mongoose'
+
+const appointmentSchema = new Schema({
+  residentId: {
+    type: ObjectId,
+    required: true,
+  },
+
+  timeslot: {
+    type: Date,
+    required: true,
+  },
+
+  status: {
+    type: String,
+    enum: ["pending", "confirmed", "visited"],
+    default: "pending"
+  },
+
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const Appointment = mongoose.models.Resident || mongoose.model("Appointment", appointmentSchema); 
+
+export default Appointment;
