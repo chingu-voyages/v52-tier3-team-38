a8cb0f9dbf54869db2c2fffef94cefc8d1df9468
RossaMania <raclettenberg@gmail.com>
2024-11-22 13:09:02 -0600

    Add routing to Header component and create empty login and signup pages

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,12 +1,19 @@
+"use client";
+
 import { Button } from "react-bootstrap"
 
+import { useRouter } from "next/navigation"
+
 const Header = () => {
+
+  const router = useRouter()
+
   return (
     <>
     <div>Header</div>
     <h1>Solarize</h1>
-    <Button variant="primary">Sign Up!</Button>
-    <Button variant="primary">Log In</Button>
+    <Button variant="primary" onClick={() => router.push("/user/signup")}>Sign Up!</Button>
+    <Button variant="primary" onClick={() => router.push("/user/login")}>Log In</Button>
     <Button variant="danger">Log Out</Button>
     </>
   )
---
+++ src/app/user/signup/page.js
@@ -0,0 +1,10 @@
+"use client";
+
+const page = () => {
+  return (
+    <div>
+    <h2>This will be sign up page.</h2></div>
+  )
+}
+
+export default page
