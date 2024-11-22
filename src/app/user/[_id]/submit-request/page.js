b3edc78c2e5e8b4cb13d21b91b82966b42ea0094
RossaMania <raclettenberg@gmail.com>
2024-11-21 22:15:56 -0600

    Add empty submit request page component for resident user submissions.

---
+++ src/app/user/[_id]/submit-request/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+    <h2>This page file is for the submission form that a resident user can submit.</h2>
+    <h3>This should be wired up to the landing page and even the nav bar in the footer with some of Submit Request button.</h3>
+    </div>
+  )
+}
+
+export default page
