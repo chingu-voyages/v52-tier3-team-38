6d7ea65c36bb0c75bfbe07e88a052086ba4dca74
RossaMania <raclettenberg@gmail.com>
2024-11-22 14:12:43 -0600

    Add GitHub link to Navbar component and clean up code formatting

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -4,11 +4,12 @@ import { Button } from "react-bootstrap";
 import { FaGithub } from "react-icons/fa";
 
 import { useRouter } from "next/navigation"
+import Link from "next/link"
 
 const Navbar = () => {
 
   const router = useRouter()
-  
+
   return (
     <>
     <div>Navbar
@@ -29,8 +30,9 @@ const Navbar = () => {
     <Button variant="primary" onClick={() => router.push("/admin/_id/schedule")}>Schedule</Button>
     <Button variant="primary" onClick={() => router.push("/about")}>About Us</Button>
     </div>
-
+    <Link href="https://github.com/chingu-voyages/v52-tier3-team-38" target="_blank">
     <FaGithub />
+    </Link>
     </>
   )
 }
