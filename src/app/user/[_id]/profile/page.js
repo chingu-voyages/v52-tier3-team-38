837bcb13fe4ae73dcfb35a0eda5935a43c6cac0c
RossaMania <raclettenberg@gmail.com>
2024-11-29 13:48:44 -0600

    Enhance Admin and User profile pages with detailed installation request information and status updates per the Trello card.

--- src/app/user/[_id]/profile/page.js
+++ src/app/user/[_id]/profile/page.js
@@ -4,7 +4,26 @@ const UserPage = () => {
   return (
     <div>
     <h2>This will be the user info page.</h2>
-    <h2>We should call it profile to keep it sleek!</h2>
+    <div>
+<h3>Here is a list of your installation requests:</h3>
+<h4></h4>
+    </div>
+    <div>
+    <h4>user.name</h4>
+    <h4>appointment.address</h4>
+    <h4>appointment.timeslot</h4>
+    <h4>Your appointment is:</h4>
+    <h4>appointment.status</h4>
+    <h4>Cancel install here.</h4>
+    </div>
+    <div>
+    <h4>user.name</h4>
+    <h4>appointment.address</h4>
+    <h4>appointment.timeslot</h4>
+    <h4>Your appointment is:</h4>
+    <h4>appointment.status</h4>
+    <h4>Cancel install here.</h4>
+    </div>
     </div>
   )
 }
