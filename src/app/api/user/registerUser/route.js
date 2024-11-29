70d84c369534c31da95731e43a8b4f4080bbbd96
Gary Smith <garysmith193358@gmail.com>
2024-11-29 13:27:18 -0500

    Commented out register user api until refactored to avoid errors for deployment

--- src/app/api/user/registerUser/route.js
+++ src/app/api/user/registerUser/route.js
@@ -1,28 +1,28 @@
-import { NextResponse } from 'next/server'
-import dbConnect from '../../../../../utils/dbConnect';
-import User from '../../../../../models/User';
+// import { NextResponse } from 'next/server'
+// import dbConnect from '../../../../../utils/dbConnect';
+// import User from '../../../../../models/User';
 
-export const POST = async(req) => { //needs be rewritten to handle transition to supabase
-  try {
-    await dbConnect();
+// export const POST = async(req) => { //needs be rewritten to handle transition to supabase
+//   try {
+//     await dbConnect();
 
-    const body = await req.json();
+//     const body = await req.json();
 
-    const userExists = await User.find({email: body.email});
+//     const userExists = await User.find({email: body.email});
 
-    if (userExists.length > 0) return NextResponse.json({error: "User with this email already exists"}, {status: 409});
+//     if (userExists.length > 0) return NextResponse.json({error: "User with this email already exists"}, {status: 409});
 
-    const user = await User.create({
-      name: body.name,
-      email: body.email,
-      address: body.address,
-      phoneNumber: body.phoneNumber,
-      role: body.role ? body.role : 0
-    })
+//     const user = await User.create({
+//       name: body.name,
+//       email: body.email,
+//       address: body.address,
+//       phoneNumber: body.phoneNumber,
+//       role: body.role ? body.role : 0
+//     })
 
-    return NextResponse.json({message: `${user.name} has been registered successfully`}, user, {status: 201});
+//     return NextResponse.json({message: `${user.name} has been registered successfully`}, user, {status: 201});
 
-  } catch (err) {
-    return NextResponse.json({error: err});
-  }
-}
+//   } catch (err) {
+//     return NextResponse.json({error: err});
+//   }
+// }
