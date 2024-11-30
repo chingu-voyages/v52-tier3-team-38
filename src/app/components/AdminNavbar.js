0377f4d8d47c14d9eec897cb0e3f0f8d4a5e9790
RossaMania <raclettenberg@gmail.com>
2024-11-30 16:48:21 -0600

    Refactor navigation components to use React Bootstrap Nav for improved structure and styling. Add Services page component.

--- src/app/components/AdminNavbar.js
+++ src/app/components/AdminNavbar.js
@@ -1,28 +1,32 @@
 "use client";
 
-import { Button } from "react-bootstrap";
+import Nav from 'react-bootstrap/Nav';
+
 import { FaGithub } from "react-icons/fa";
 
-import { useRouter } from "next/navigation"
 import Link from "next/link"
 
 const AdminNavbar = () => {
 
-  const router = useRouter()
-
   return (
-    <>
+    <Nav fill variant="pills" defaultActiveKey={"/admin/_id/profile"}>
     <div>Admin Navbar</div>
-    <div>
-    <Button variant="primary" onClick={() => router.push("/admin/_id/profile")}>Profile</Button>
-    <Button variant="primary" onClick={() => router.push("/admin/_id/requests")}>Requests</Button>
-    <Button variant="primary" onClick={() => router.push("/admin/_id/schedule")}>Schedule</Button>
-    <Button variant="primary" onClick={() => router.push("/about")}>About Us</Button>
-    </div>
+    <Nav.Item>
+    <Nav.Link href="/admin/_id/profile">Profile</Nav.Link>
+    </Nav.Item>
+     <Nav.Item>
+    <Nav.Link href="/admin/_id/requests">Requests</Nav.Link>
+    </Nav.Item>
+     <Nav.Item>
+      <Nav.Link href="/admin/_id/schedule">Schedule</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link href="/about">About Us</Nav.Link>
+    </Nav.Item>
     <Link href="https://github.com/chingu-voyages/v52-tier3-team-38" target="_blank">
     <FaGithub />
     </Link>
-    </>
+    </Nav>
   )
 }
 
--- src/app/components/UserNavbar.js
+++ src/app/components/UserNavbar.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "react-bootstrap";
+import { Button, Nav } from "react-bootstrap";
 import { FaGithub } from "react-icons/fa";
 
 import { useRouter } from "next/navigation"
@@ -11,18 +11,24 @@ const UserNavbar = () => {
   const router = useRouter()
 
   return (
-    <>
+    <Nav fill variant="pills" defaultActiveKey={"/user/_id/profile"}>
     <div>Navbar</div>
-    <div>
-    <Button variant="primary" onClick={() => router.push("/user/_id/profile")}>Profile</Button>
-    <Button variant="primary" onClick={() => router.push("/user/_id/submit-request")}>Submit Request</Button>
-    <Button variant="primary" onClick={() => router.push("/user/_id/service-history")}>Service History</Button>
-    <Button variant="primary" onClick={() => router.push("/about")}>About Us</Button>
-    </div>
+    <Nav.Item>
+      <Nav.Link href="/user/_id/profile">Profile</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+    <Nav.Link href="/user/_id/submit-request">Submit Request</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+    <Nav.Link href="/user/_id/service-history">Service History</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+    <Nav.Link href="/about">About Us</Nav.Link>
+    </Nav.Item>
     <Link href="https://github.com/chingu-voyages/v52-tier3-team-38" target="_blank">
     <FaGithub />
     </Link>
-    </>
+    </Nav>
   )
 }
 
