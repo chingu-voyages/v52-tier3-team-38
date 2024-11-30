194f1b961a755c2e4c5f504f4bf4b449a6868ccd
RossaMania <raclettenberg@gmail.com>
2024-11-30 15:40:00 -0600

    Refactor logout functionality in AdminHeader and UserHeader components to use new client-side logic.

--- src/app/components/UserHeader.js
+++ src/app/components/UserHeader.js
@@ -1,16 +1,23 @@
 "use client";
 
-import { Button } from "react-bootstrap"
-import { logout } from "../../../utils/supabase/actions";
+import { Button } from "react-bootstrap";
+import { useRouter } from "next/navigation";
+import { clientLogout } from "../../../utils/supabase/client-logout";
 
 const UserHeader = () => {
+
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await clientLogout(router); // Call the client-side logout logic
+  };
 
   return (
     <>
     <div>Header</div>
     <h1>Solarize</h1>
     <h3>Logged in as resident!</h3>
-    <Button variant="danger" onClick={() => logout()}>Log Out</Button>
+    <Button variant="danger" onClick={handleLogout}>Log Out</Button>
     </>
   )
 }
