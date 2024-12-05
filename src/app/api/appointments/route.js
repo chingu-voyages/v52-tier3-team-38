1693cd6c0e86f2975d2eced4926bb4eb3402af7a
Gary Smith <garysmith193358@gmail.com>
2024-12-05 16:16:28 -0500

    Created api to handle removal of appointments

--- src/app/api/appointments/route.js
+++ src/app/api/appointments/route.js
@@ -12,3 +12,20 @@ export const GET = async() => {
     return NextResponse.json({error})
   }
 }
+
+export const DELETE = async(request) => { //handle cancelled appointments
+    const supabase = await createClient();
+
+    const body = await request.json(); 
+    const { id } = body
+
+    const response = await supabase.from("appointments").delete("*").eq("resident_id", id);
+
+    if (response.status != 204) {
+      return NextResponse.json({error: response.error})
+    }
+
+    return NextResponse.json({success: true, message: 'Appointment was successfully deleted'}, {status: 200});
+
+}
+
