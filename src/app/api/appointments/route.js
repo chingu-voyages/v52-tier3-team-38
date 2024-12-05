25d7061032de8c111822917160b7338aa9c0a826
Gary Smith <garysmith193358@gmail.com>
2024-12-05 17:00:02 -0500

    Created API to handle updating appointment info

--- src/app/api/appointments/route.js
+++ src/app/api/appointments/route.js
@@ -29,3 +29,33 @@ export const DELETE = async(request) => { //handle cancelled appointments
 
 }
 
+
+export const PUT = async(request) => { //Looking to make this useful for both users updating info and admins updating status
+  const supabase = await createClient();
+
+  const body = await request.json(); 
+  const { appointmentId } = body;
+
+  console.log(appointmentId)
+
+  const { data: appointment, error} = await supabase.from('appointments').select("*").eq("id", appointmentId).single()
+
+  if (error) {
+    return NextResponse.json({error}, {status: 404})
+  }
+
+  const appointmentBody = {
+      timeslot: body.timeSlot ? body.timeSlot: appointment.timeSlot,
+      address:  body.address ? body.address : appointment.address,
+      email: body.email ? body.email : appointment.email,
+      status: body.status ? body.status : appointment.status
+  }
+
+  const response = await supabase.from('appointments').update(appointmentBody).eq("id", appointmentId).select("*")
+
+  if (response.error) {
+    return NextResponse.json({error: response.error}, {status: 400})
+  }
+
+  return NextResponse.json({success: true, message: "Appointment successfully updated", appointment: response.data})
+}
