04ee2666fe86c33c9f5acf4d8341543cbffa4ef3
Gary Smith <garysmith193358@gmail.com>
2024-12-07 14:49:49 -0500

    Adjusted addressSuggestions api to remove coordinates from api results, only returning addresses

--- src/app/api/addressSuggestions/route.js
+++ src/app/api/addressSuggestions/route.js
@@ -11,7 +11,7 @@ export const POST = async(request) => {
     const addressNumber = addressInfo[0] * 1 || null
     const addressName = addressInfo[1] || null
 
-    const response = await fetch(`https://data.lacity.org/resource/4ca8-mxuh.json?$query=SELECT%20%60hse_nbr%60,%20%60str_nm%60,%20%60str_sfx_cd%60,%20%60zip_cd%60,%20%60lat%60,%20%60lon%60%20WHERE%20%60hse_nbr%60%20=%20${addressNumber}%20OR%20LOWER(%60str_nm%60)%20LIKE%20%22%25${addressName}%25%22%20LIMIT%2010`)
+    const response = await fetch(`https://data.lacity.org/resource/4ca8-mxuh.json?$query=SELECT%20%60hse_nbr%60,%20%60str_nm%60,%20%60str_sfx_cd%60,%20%60zip_cd%60%20WHERE%20%60hse_nbr%60%20=%20${addressNumber}%20OR%20LOWER(%60str_nm%60)%20LIKE%20%22%25${addressName}%25%22%20LIMIT%2010`);
 
     const data = await response.json();
 
@@ -19,16 +19,14 @@ export const POST = async(request) => {
   //     "hse_nbr": "1769",
   //     "str_nm": "IVAR",
   //     "str_sfx_cd": "AVE",
-  //     "zip_cd": "90028",
-  //     "lat": "34.10369",
-  //     "lon": "-118.32831"
+  //     "zip_cd": "90028"
   // },
 
     // Putting together the addresses 
     const addressData = data.map(addressRow => {
-      const {hse_nbr, str_nm, str_sfx_cd, zip_cd, lat, lon} = addressRow
+      const {hse_nbr, str_nm, str_sfx_cd, zip_cd} = addressRow
       const address = `${hse_nbr} ${str_nm} ${str_sfx_cd} ${zip_cd}`
-      return {address: address, coordinates: {lat, lon}}
+      return address
     })
 
     return NextResponse.json({addressData})
