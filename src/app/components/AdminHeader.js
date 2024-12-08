0057ab390831a6ed27aed329c00ab075061cd10c
Gary Smith <garysmith193358@gmail.com>
2024-12-08 16:22:11 -0500

    Deleted client-logout and refactored admin logout to fix bug prevent logout from working correctly

--- src/app/components/AdminHeader.js
+++ src/app/components/AdminHeader.js
@@ -1,26 +1,18 @@
 "use client";
 
 import { Button } from "react-bootstrap";
-import { useRouter } from "next/navigation";
-import { clientLogout } from "../../../utils/supabase/client-logout";
+import { logout } from "../../../utils/supabase/actions";
 import Link from "next/link";
 
 const AdminHeader = () => {
-
-  const router = useRouter();
-
-  const handleLogout = async () => {
-    await clientLogout(router); // Call the client-side logout logic
-  };
-
   return (
     <>
     <div>Header</div>
     <Link href="/"><h1>Solarize</h1></Link>
     <h3>Logged in as admin!</h3>
-    <Button variant="danger" onClick={handleLogout}>Log Out</Button>
+    <Button variant="danger" onClick={logout}>Log Out</Button>
     </>
   )
 }
 
-export default AdminHeader
+export default AdminHeader;
