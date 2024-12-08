675a68afa8d9d15b87c29a790745187891161f99
Gary Smith <garysmith193358@gmail.com>
2024-12-08 12:23:31 -0500

    Adjusted bookAppointment api by changing order of checks/validation, included lat and lon positions to appointment body to use for map view

--- src/app/api/user/[userId]/bookAppointment/route.js
+++ src/app/api/user/[userId]/bookAppointment/route.js
@@ -7,6 +7,10 @@ export const POST = async(request, { params }) => {
     const body = await request.json();
     const { userId } = await params;
 
+    const { data: user, error } = await supabase.from("user_details").select("*").eq("id", userId).single();
+
+    if (error) return NextResponse.json({error: "A user with this id does not exist "}, {status: 404});
+
     const alreadyHasAppointment = await supabase.from("appointments").select("*").eq("resident_id", userId)
 
     if (alreadyHasAppointment.data.length > 0) return NextResponse.json({error: `Error: You already have an appointment booked.`}, {status: 403});
@@ -19,14 +23,9 @@ export const POST = async(request, { params }) => {
     const checkAddress = await fetch(`https://data.lacity.org/resource/4ca8-mxuh.json?$query=SELECT%20%60hse_nbr%60,%20%60str_nm%60,%20%60str_sfx_cd%60,%20%60zip_cd%60,%20%60lat%60,%20%60lon%60%20WHERE%20%60hse_nbr%60=${addressNumber}%20AND%20LOWER(%60str_nm%60)=LOWER('${addressName}')%20LIMIT%2010`);
     const validAddress = await checkAddress.json();
 
-
     if (validAddress.length === 0) {
       return NextResponse.json({error: "The address submitted is not a valid address in LA. Please try again."}, {status:404})
     }
-
-    const { data: user, error } = await supabase.from("user_details").select("*").eq("id", userId).single();
-
-    if (error) return NextResponse.json({error: "A user with this id does not exist "}, {status: 404});
 
     // if (user.role === 1) return NextResponse.json({error: "Admins cannot book appointments."}, {status: 403});
 
@@ -35,7 +34,8 @@ export const POST = async(request, { params }) => {
       timeslot: body.timeslot,
       address:  body.address,
       email: user.email,
-      // include coordinates into database
+      lat: parseFloat(validAddress[0].lat), // to use for google map coordinates in admin interface
+      lon: parseFloat(validAddress[0].lon)
     })
 
     return NextResponse.json({ message: `Appointment for ${user.name} sucessfully created`, appointmentInsertInfo }, { status: 201 });
