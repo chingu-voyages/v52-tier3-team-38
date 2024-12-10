119785553488d6aaa4ca2bd5f1f28a3205bbccd5
RossaMania <raclettenberg@gmail.com>
2024-12-10 15:27:53 -0600

    Refactor UserNavbar to use Bootstrap React Navbar and NavDropdown for improved navigation.

--- src/app/components/UserNavbar.js
+++ src/app/components/UserNavbar.js
@@ -2,44 +2,35 @@
 
 import { useParams } from "next/navigation"
 import Link from "next/link"
-import { Nav } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { FaGithub } from "react-icons/fa";
-import {  PiUserDuotone, PiNotePencilDuotone, PiQuestionMarkFill, PiCompassRoseDuotone } from "react-icons/pi";
+import {  PiUserDuotone, PiNotePencilDuotone, PiQuestionMarkFill, PiSunBold } from "react-icons/pi";
 
 const UserNavbar = () => {
   const { _id } = useParams();
 
   return (
-    <div>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary dropdown">
-  <div class="container-fluid">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <PiCompassRoseDuotone />
-          </a>
-          <ul class="dropdown-menu">
-            <li><a className="dropdown-item" href={`/user/${_id}/profile`}><PiUserDuotone />Profile</a></li>
-            <li><a className="dropdown-item" href={`/user/${_id}/submit-request`}><PiNotePencilDuotone />Submit Request</a></li>
-            <li><a className="dropdown-item" href="/about"><PiQuestionMarkFill />About Us</a></li>
-            <li><a className="dropdown-item" href="https://github.com/chingu-voyages/v52-tier3-team-38" target="_blank"><FaGithub /></a></li>
-          </ul>
-    </div>
-</nav>
 
-    {/* <Nav.Item>
-      <Nav.Link href={`/user/${_id}/profile`}>Profile</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
 
-    <Nav.Link href={`/user/${_id}/submit-request`}>Submit Request</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-    <PiQuestionMarkFill />
-    <Nav.Link href="/about">About Us</Nav.Link>
-    </Nav.Item>
-    <Link href="https://github.com/chingu-voyages/v52-tier3-team-38" target="_blank">
-    <FaGithub />
-    </Link> */}
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/"><PiSunBold className="me-2" />Solarize</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href={`/user/${_id}/profile`}><PiUserDuotone className="me-2" />Profile</NavDropdown.Item>
+              <NavDropdown.Item href={`/user/${_id}/submit-request`}>
+                <PiNotePencilDuotone className="me-2" />Submit Request
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/about"><PiQuestionMarkFill className="me-2" />About Us</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="https://github.com/chingu-voyages/v52-tier3-team-38" target="_blank"><FaGithub className="me-2" />GitHub Repo</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
