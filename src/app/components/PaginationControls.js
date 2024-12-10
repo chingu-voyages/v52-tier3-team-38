52179b3baff7a399a616684547c0607fbff6033f
Gary Smith <garysmith193358@gmail.com>
2024-12-09 20:23:04 -0500

    Created pagination controls component to help render different data when page is moved to next and previous page

--- src/app/components/PaginationControls.js
+++ src/app/components/PaginationControls.js
@@ -1,7 +1,40 @@
+"use client"
+
 import React from "react";
+import { useRouter, useSearchParams } from 'next/navigation';
 
-const PaginationControls = () => {
+const PaginationControls = ({ hasNextPage, hasPrevPage }) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-}
+  const page = searchParams.get('page') || '1';
+  const per_page = searchParams.get('per_page') || '2';
+
+  const pathName = `/admin/requests`;
+
+  return (
+    <div className="d-flex gap-2 align-items-center">
+      <button 
+        className="btn btn-primary" 
+        disabled={!hasPrevPage} 
+        onClick={() => {
+          router.push(`${pathName}/?page=${Number(page) - 1}&per_page=${per_page}`);
+        }}
+      >
+        Prev Page
+      </button>
+
+      <div>{page} / {Math.ceil(10 / Number(per_page))}</div>
+
+      <button 
+        className="btn btn-primary" 
+        disabled={!hasNextPage} 
+        onClick={() => router.push(`${pathName}?page=${Number(page) + 1}&per_page=${per_page}`)}
+      >
+        Next Page
+      </button>
+    </div>
+  );
+};
 
 export default PaginationControls;
