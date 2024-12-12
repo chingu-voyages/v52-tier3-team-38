e4cb7d037eee5ee3085cb9c7264ba0adcf8558e0
RossaMania <raclettenberg@gmail.com>
2024-12-11 21:28:20 -0600

    refactor(headers): Remove and delete unused header components.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,12 @@
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import UserHeader from "./components/UserHeader";
 import UserNavbar from "./components/UserNavbar";
-import UnauthHeader from "./components/UnauthHeader";
 import UnauthNavbar from "./components/UnauthNavbar";
 
 import { isAdmin } from "../../utils/supabase/isAdmin";
 
 import { createClient } from "../../utils/supabase/server";
-import AdminHeader from "./components/AdminHeader";
 import AdminNavbar from "./components/AdminNavbar";
 
 export const metadata = {
